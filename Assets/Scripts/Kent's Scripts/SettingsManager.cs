3cdba2b751d177adf3df8adf47b64f6c8d825d6d
chainlynx42 <chainlynx42@gmail.com>
2020-03-14 22:45:11 -0700

    Changed menu music, added button SFX, added settings persistency

---
+++ Assets/Scripts/Kent's Scripts/SettingsManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour
+{
+
+    public TMPro.TMP_Dropdown langDrop;
+    public TMPro.TMP_Dropdown gramDrop;
+    public Slider volumeSlider;
+    public AudioMixer mixer;
+
+    private void Start()
+    {
+        mixer.SetFloat("volume", PlayerPrefs.GetFloat("volume"));
+        langDrop.value = PlayerPrefs.GetInt("tLanguage");
+        gramDrop.value = PlayerPrefs.GetInt("grammar");
+        volumeSlider.value = -10f;// PlayerPrefs.GetFloat("volume");
+    }
+
+    public void saveLanguage(int language)
+    {
+        PlayerPrefs.SetInt("tLanguage", language);
+    }
+
+    public void saveGrammar(int grammar)
+    {
+        PlayerPrefs.SetInt("grammar", grammar);
+    }
+
+    public void saveVolume(float vol)
+    {
+        PlayerPrefs.SetFloat("volume", vol);
+        mixer.SetFloat("volume", vol);
+    }
+}
---
+++ Assets/Scripts/Kent's Scripts/userData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userData : MonoBehaviour
+{
+    //enum languages { "English", "Spanish", "French"};
+
+    public int nativeLanguage;
+    public int targetLanguage;
+    //public int points;
+    //public float volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nativeLanguage = PlayerPrefs.GetInt("nLanguage");
+        targetLanguage = PlayerPrefs.GetInt("language");
+        //volume = PlayerPrefs.GetFloat("volume");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
