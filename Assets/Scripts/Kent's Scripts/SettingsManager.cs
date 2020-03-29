77b71241e48fb20f6f16b5ae0d096ec54d321fb1
chainlynx42 <chainlynx42@gmail.com>
2020-03-28 21:40:32 -0700

    added a multiple choice vocab review, added scene transitions

--- Assets/Scripts/Kent's Scripts/SettingsManager.cs
+++ Assets/Scripts/Kent's Scripts/SettingsManager.cs
@@ -9,13 +9,26 @@ public class SettingsManager : MonoBehaviour
     public TMPro.TMP_Dropdown gramDrop;
     public Slider volumeSlider;
     public AudioMixer mixer;
+    public int signedIn = 0;
+    public GameObject logIn;
+    public GameObject overLay;
+    public GameObject menu;
 
     private void Start()
     {
         mixer.SetFloat("volume", PlayerPrefs.GetFloat("volume"));
         langDrop.value = PlayerPrefs.GetInt("tLanguage");
         gramDrop.value = PlayerPrefs.GetInt("grammar");
-        volumeSlider.value = -10f;// PlayerPrefs.GetFloat("volume");
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+        signedIn = PlayerPrefs.GetInt("Authentication");
+        Debug.Log("Started");
+        if (signedIn == 1)
+        {
+            Debug.Log("Did it");
+            logIn.SetActive(false);
+            overLay.SetActive(true);
+            menu.SetActive(true);
+        }
     }
 
     public void saveLanguage(int language)
@@ -32,5 +45,25 @@ public class SettingsManager : MonoBehaviour
     {
         PlayerPrefs.SetFloat("volume", vol);
         mixer.SetFloat("volume", vol);
+    }
+    
+    public void saveAuthentication()
+    {
+        Debug.Log("Saved");
+        PlayerPrefs.SetInt("Authentication", signedIn);
+    }
+
+    public void authenticate()
+    {
+        Debug.Log("Signed On");
+        signedIn = 1;
+        saveAuthentication();
+    }
+
+    public void signOut()
+    {
+        Debug.Log("Signed off");
+        signedIn = 0;
+        saveAuthentication();
     }
 }
---
+++ Assets/Scripts/Kent's Scripts/goToScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goToScene : MonoBehaviour
+{
+    public void toMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+    public void toPlayground()
+    {
+        SceneManager.LoadScene("Playground", LoadSceneMode.Single);
+    }
+
+    public void toVocabReview()
+    {
+        SceneManager.LoadScene("VocabReview", LoadSceneMode.Single);
+    }
+}
