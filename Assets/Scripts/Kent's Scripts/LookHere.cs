a14cd42e13e9704dc754c2475d4bc29359b2029c
chainlynx42 <chainlynx42@gmail.com>
2020-04-23 23:43:49 -0700

    yeo

---
+++ Assets/Scripts/Kent's Scripts/LookHere.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookHere : MonoBehaviour
+{
+    public Transform cameraLocation;
+    public Transform cameraTarget;
+    public string[] answers;
+    public Transform sam;
+    public float smoothSpeed = .125f;
+    [SerializeField]
+    private LookHere[] lookHeres;
+
+    public bool checkAnswer(string guess, int languageIndex)
+    {
+        if (answers[languageIndex].ToLower().Replace(".", "") == guess.ToLower().Replace(".", "")) return true;
+        return false;
+    }
+
+    public void lookAtMe(Transform cam)
+    {
+        sam = cam;
+    }
+
+    void LateUpdate()
+    {
+        sam.LookAt(cameraTarget);
+        Vector3 nextPosition = Vector3.Lerp(sam.position, cameraLocation.position, smoothSpeed);
+        sam.position = nextPosition;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(cameraLocation.position, cameraTarget.position);
+    }
+}
--- Assets/Scripts/Kent's Scripts/PP_Engine.cs
+++ Assets/Scripts/Kent's Scripts/PP_Engine.cs
@@ -9,120 +9,120 @@ using UnityEngine.UI;
 //instead try making a component to attach to a game object that will provide the needed data and form a node in a linked list in a series
 //create an end for the series
 
-public class question //This is intended to encapsulate a single question/translation
-{
-    string[] questionText;//This array contains the translations in different languages and should always be in the following order
-    //ENGLISH, SPANISH, ... TBD
-    public Transform targetCameraLocation; //The camera will move to this location when the question is active
-    public Transform cameraTarget; //The camera will face this location when the question is active
-    public question(string[] texts)
-    {
-        questionText = texts;
-    }
+//public class question //This is intended to encapsulate a single question/translation
+//{
+//    string[] questionText;//This array contains the translations in different languages and should always be in the following order
+//    //ENGLISH, SPANISH, ... TBD
+//    public Transform targetCameraLocation; //The camera will move to this location when the question is active
+//    public Transform cameraTarget; //The camera will face this location when the question is active
+//    public question(string[] texts)
+//    {
+//        questionText = texts;
+//    }
 
-    public string getText(int languageIndex)
-    {
-        return questionText[languageIndex];
-    }
+//    public string getText(int languageIndex)
+//    {
+//        return questionText[languageIndex];
+//    }
 
-    public Transform getTarget()
-    {
-        return targetCameraLocation;
-    }
-}
+//    public Transform getTarget()
+//    {
+//        return targetCameraLocation;
+//    }
+//}
 
-public class questionSeries //This class is for handling a series of related questions in a specific order such as "I am slicing bread." before "I am toasting bread."
-{
-    public question[] series;
-    int current = -1;
+//public class questionSeries //This class is for handling a series of related questions in a specific order such as "I am slicing bread." before "I am toasting bread."
+//{
+//    public question[] series;
+//    int current = -1;
 
-    public questionSeries(question first)
-    {
-        series[0] = first;
-    }
+//    public questionSeries(question first)
+//    {
+//        series[0] = first;
+//    }
 
-    public void addQuestion(question adding)
-    {
-        series[series.Length] = adding;
-    }
-    //public void nextQuestion()
-    //{
-    //    current++;
-    //}
+//    public void addQuestion(question adding)
+//    {
+//        series[series.Length] = adding;
+//    }
+//    //public void nextQuestion()
+//    //{
+//    //    current++;
+//    //}
 
-    public question getQuestion()
-    {
-        if(current + 1 == series.Length)
-        {
-            return null;
-        }
-        return series[current++];
-    }
-}
+//    public question getQuestion()
+//    {
+//        if(current + 1 == series.Length)
+//        {
+//            return null;
+//        }
+//        return series[current++];
+//    }
+//}
 
-public class questionHandler //This class is for interfacing with a game controller to get the proper question data
-{
-    private int nativeLanguage;
-    private int targetLanguage;
-    private question currentQuestion;
-    public questionSeries[] allQuestions;
-    System.Random rng = new System.Random();
-    private int[] usedNumbers; //series that have already been used
+//public class questionHandler //This class is for interfacing with a game controller to get the proper question data
+//{
+//    private int nativeLanguage;
+//    private int targetLanguage;
+//    private question currentQuestion;
+//    public questionSeries[] allQuestions;
+//    System.Random rng = new System.Random();
+//    private int[] usedNumbers; //series that have already been used
 
-    public questionHandler(int nat, int targ, questionSeries[] al)
-    {
-        nativeLanguage = nat;
-        targetLanguage = targ;
-        allQuestions = al;
-        usedNumbers[usedNumbers.Length - 1] = rng.Next() % allQuestions.Length;
-        currentQuestion = allQuestions[usedNumbers[usedNumbers.Length - 1]].getQuestion();
-    }
+//    public questionHandler(int nat, int targ, questionSeries[] al)
+//    {
+//        nativeLanguage = nat;
+//        targetLanguage = targ;
+//        allQuestions = al;
+//        usedNumbers[usedNumbers.Length - 1] = rng.Next() % allQuestions.Length;
+//        currentQuestion = allQuestions[usedNumbers[usedNumbers.Length - 1]].getQuestion();
+//    }
 
-    public void next()
-    {
-        currentQuestion = allQuestions[usedNumbers[usedNumbers.Length - 1]].getQuestion();
-        if (currentQuestion == null)
-        {
-            int num = rng.Next() % allQuestions.Length;
-            bool used = true;
-            while (used)
-            {
-                used = false;
-                foreach (int i in usedNumbers)
-                {
-                    if(i == num)
-                    {
-                        num = rng.Next() % allQuestions.Length;
-                        used = true;
-                        break;
-                    }
-                }
-            }
-            usedNumbers[usedNumbers.Length] = num;
-            currentQuestion = allQuestions[usedNumbers.Length - 1].getQuestion();
-        }
-    }
+//    public void next()
+//    {
+//        currentQuestion = allQuestions[usedNumbers[usedNumbers.Length - 1]].getQuestion();
+//        if (currentQuestion == null)
+//        {
+//            int num = rng.Next() % allQuestions.Length;
+//            bool used = true;
+//            while (used)
+//            {
+//                used = false;
+//                foreach (int i in usedNumbers)
+//                {
+//                    if(i == num)
+//                    {
+//                        num = rng.Next() % allQuestions.Length;
+//                        used = true;
+//                        break;
+//                    }
+//                }
+//            }
+//            usedNumbers[usedNumbers.Length] = num;
+//            currentQuestion = allQuestions[usedNumbers.Length - 1].getQuestion();
+//        }
+//    }
 
-    public string getTarget()
-    {
-        return currentQuestion.getText(targetLanguage);
-    }
+//    public string getTarget()
+//    {
+//        return currentQuestion.getText(targetLanguage);
+//    }
 
-    public string getNative()
-    {
-        return currentQuestion.getText(nativeLanguage);
-    }
+//    public string getNative()
+//    {
+//        return currentQuestion.getText(nativeLanguage);
+//    }
 
-    public Transform getCameraTargetLocation()
-    {
-        return currentQuestion.targetCameraLocation;
-    }
+//    public Transform getCameraTargetLocation()
+//    {
+//        return currentQuestion.targetCameraLocation;
+//    }
 
-    public Transform getCameraTarget()
-    {
-        return currentQuestion.cameraTarget;
-    }
-}
+//    public Transform getCameraTarget()
+//    {
+//        return currentQuestion.cameraTarget;
+//    }
+//}
 
 public class PP_Engine : MonoBehaviour
 {
@@ -138,19 +138,19 @@ public class PP_Engine : MonoBehaviour
     public TMPro.TextMeshProUGUI score;
     public Transform cam;
 
-    public questionHandler questionEngine;
+    //public questionHandler questionEngine;
 
     //public Transform[] targets;
 
     // Start is called before the first frame update
     void Start()
     {
-        questionSeries ingredients = new questionSeries(new question(new string[] { "apple", "manzana"}));
-        ingredients.addQuestion(new question(new string[] { "potato", "papa" }));
-        ingredients.addQuestion(new question(new string[] { "bread", "pan" }));
-        questionEngine = new questionHandler(0, PlayerPrefs.GetInt("tLanguage", 1), new questionSeries[] { ingredients });
-        cam.position = questionEngine.getCameraTargetLocation().position;
-        cam.LookAt(questionEngine.getCameraTarget());
+        //questionSeries ingredients = new questionSeries(new question(new string[] { "apple", "manzana"}));
+        //ingredients.addQuestion(new question(new string[] { "potato", "papa" }));
+        //ingredients.addQuestion(new question(new string[] { "bread", "pan" }));
+        //questionEngine = new questionHandler(0, PlayerPrefs.GetInt("tLanguage", 1), new questionSeries[] { ingredients });
+        //cam.position = questionEngine.getCameraTargetLocation().position;
+        //cam.LookAt(questionEngine.getCameraTarget());
         QANum = rnd.Next(5);
         points = 0;
     }
